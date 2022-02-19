daae311e32637ea094a6e5623a1439c0b294d7cc
smoonmare <smoonmare@gmail.com>
2022-02-19 08:39:38 -0800

    Created BetterDirective via 'ng g d' and used Renderer2 to render style in AppComponent

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -3,7 +3,6 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
 @Directive({
   selector: '[appBasicHighlight]'
 })
-
 export class BasicHighlightDirective implements OnInit{
   
   constructor(private elementRef: ElementRef) {}
